eef2b528410495a66edb5bcbf3aa077a07f4c707
AmarNagim <99069493@mydavinci.nl>
2021-09-08 09:47:46 +0200

    input en if-statement

---
+++ meten_is_weten.py
@@ -0,0 +1,21 @@
+# meten is weten
+# twee getallen vergelijken
+
+getal_1 = int(input('Schrijf hier getal 1: '))
+getal_2 = int(input('Schrijf hier getal 2: '))
+max = int()
+min = int()
+
+if getal_1 > getal_2:
+    max = getal_1
+    print(f'Getal 1 is het grootste getal: {max}.')
+else:
+    max = getal_2
+    print(f'Getal 2 is het grootste getal: {max}.')
+
+
+
+
+   
+    
+
