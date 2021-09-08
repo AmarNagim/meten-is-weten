4465728ba0318ab9e38837d8b0c110bdad7e7605
AmarNagim <99069493@mydavinci.nl>
2021-09-08 10:01:24 +0200

    min en max

--- meten_is_weten.py
+++ meten_is_weten.py
@@ -5,13 +5,34 @@ getal_1 = int(input('Schrijf hier getal 1: '))
 getal_2 = int(input('Schrijf hier getal 2: '))
 max = int()
 min = int()
+same = int()
 
 if getal_1 > getal_2:
     max = getal_1
-    print(f'Getal 1 is het grootste getal: {max}.')
+    min = getal_2
+    print(f'Het maximum is: {max}.')
+    print(f'Het minimum is: {min}.')
 else:
     max = getal_2
-    print(f'Getal 2 is het grootste getal: {max}.')
+    min = getal_1
+    print(f'Het maximum is: {max}.')
+    print(f'Het minimum is: {min}.')    
+  
+
+####################################
+######   VORIGE UITBREIDING   ######
+####################################
+#if getal_1 > getal_2:
+#    max = getal_1
+#    print(f'Getal 1 is het grootste getal: {max}.')
+# elif getal_1 < getal_2:
+#    min = getal_2
+#    print(f'Getal 1 is het kleinste getal: {min}.')
+#else:
+#    if getal_1 == getal_2:
+#      same = getal_1
+#      print(f'Getal 1 en getal 2 zijn even groot: {same}')
+    
 
 
 
